ca0fd305d202d5af9fa2517087a75323f538dec7
Sean Blake <sean.blake97@outlook.com>
2022-01-09 21:05:20 -0500

    Initial Commit

---
+++ Model Room/Model_RoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Model_RoomApp.swift
+//  Model Room
+//
+//  Created by SEAN BLAKE on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Model_RoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
